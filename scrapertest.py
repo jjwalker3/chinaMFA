e94a9de6d6a8b94ed75314b497250c8e75f9e456
JW <jjwalker3@users.noreply.github.com>
2019-01-28 00:56:23 +0300

    s

---
+++ scrapertest.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import os
+import psycopg2
+
+def main():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options, executable_path="geckodriver.exe")
+        
+    driver.get("https://www.bbc.com/news")
+    html = driver.page_source
+    soup=BeautifulSoup(html, 'lxml')
+    pageText = soup.find(class_='gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-paragon-bold nw-o-link-split__anchor').text
+    
+    
+    conn = psycopg2.connect(host="dbtest123.cofdbub8go3o.eu-west-1.rds.amazonaws.com",
+                        database="dbtestname",
+                        user="tokyo6",
+                        password="shinjiikari")
+
+    cur = conn.cursor()
+
+    command = """
+    INSERT INTO testtable(content,datetime)
+    VALUES(%s,current_timestamp);
+    """
+    
+
+    cur.execute(command,(pageText,))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
